71dd380ef9c38efa23c3de47037a382b4a7cc91f
maoku <struggle_acm@126.com>
2023-04-14 21:36:34 +0800

    未完待续

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # Shellcode_Generator
-IDA Python script for generating Windows x86 shellcode with one click
+一键生成windows x86 shellcode的python脚本
+
+## 📖目录
+
+* [👨🏻‍💻  预览演示](#-预览演示)
+* [🔥 介绍](#-介绍)
+* [🔗 使用环境](#-使用环境)
+* [💡用法 Usage](#-用法-usage)
+* [📜 版权与免责声明](#-版权与免责声明)
+
+## 👨🏻‍💻 预览演示
+
+![screenshots](.\res\screenshots.gif)
+
+## 🔥 介绍
+
+​	本项目主要想解决手动编写shellcode的过程中痛点问题（**全局变量**、**重定位**、**系统API调用**等），帮助用户使用**C/C++**实现的方法一键转换成shellcode。
+
+### 优点和不足之处
+
+---
+
+**优点：**
+
+- 大胆使用全局变量和系统API，再也不用担心地址重定位问题
+
+- 不局限于MSF和CS提供shellcode模板、随意发挥。
+
+- 支持嵌套使用shellcode
+
+**不足：**
+
+- 暂不支持x64的shellcode生成
+- 对于有些VC运行时的函数处理不了，比如strtok函数
+
+## 🔗使用环境
+
+- 项目目前仅支持生成**x86**的shellcode
+
+- 需要安装**IDA**软件并支持运行**python**脚本
+
+- 使用VS编译项目时需要**禁用优化**并把代码生成设置为**多线程/MT**
+
+## 💡用法 Usage
+
+1. 打开VS项目，在Shellcode_Generator_Demo.c文件中的strat函数处添加需要生成的shellcode代码，编译项目
+2. 使用IDA打开编译好的程序，一路默认选项，可参考演示实例
+3. 在IDA中找到main函数，光标选中main函数内任意地址
+4. 按下快捷键ALT+F7，选择项目中ida_shellcode_generator.py脚本
+5. 运行结束后会生成一个shellcode文件路径和大小，可以生成raw或txt格式（需要修改脚本中的outType）
+6. 可以使用Shellcode_Generator_Demo.c中的testShellcodeRun方法测试shellcode是否可用
+
---
+++ ida_shellcode_generator.py
@@ -0,0 +1,356 @@
+import idc
+import idautils
+import ida_bytes
+import ida_nalt
+import idaapi
+
+import ctypes
+
+
+class AddrInfoStruct(ctypes.Structure):
+    _fields_ = [
+        ("originalAddr", ctypes.c_ulong),
+        ("newOffset", ctypes.c_ushort)
+    ]
+
+    def __getitem__(self, key):
+        if key == 'originalAddr':
+            return self.originalAddr
+        elif key == 'newOffset':
+            return self.newOffset
+        else:
+            raise KeyError(f'Invalid key: {key}')
+
+
+shellcodeData = bytearray()
+funcInfo_map = {}
+varInfo_map = {}
+importInfo_map = {}
+iatInfo_map = {}
+relocTable = []
+fileOffset = 0
+dllName = ""
+
+
+def addr_align():
+    global fileOffset
+
+    if fileOffset % 4 != 0:
+        align = 4 - fileOffset % 4
+        shellcodeData.extend(b'\x00' * align)
+        fileOffset += align
+
+
+def searchValue(addr, value):
+    i = 0
+    while ida_bytes.get_dword(addr + i) != value:
+        i = i + 1
+
+    return addr + i
+
+
+def patchDword(index, value):
+    dword_bytes = struct.pack('<I', value)
+    shellcodeData[index:index + 4] = dword_bytes
+
+
+def replaceMarkValue(markValue, replaceValue):
+    binary_str = struct.pack(">I", markValue)
+    hex_str = binary_str.hex()
+
+    for key, value in funcInfo_map.items():
+        func_items = FuncItems(key)
+        for addr in func_items:
+            str = idc.generate_disasm_line(addr, 0)
+            if hex_str in str and "mov" in str:
+                patchDword(idaAddr2FileOffset(searchValue(addr, markValue)), replaceValue)
+                return
+
+
+def memset(index, value, size):
+    for i in range(size):
+        shellcodeData[i + index] = value
+
+
+def idaAddr2FileOffset(idaAddr):
+    func = ida_funcs.get_func(idaAddr)
+    start_addr = func.start_ea
+
+    if start_addr not in funcInfo_map:
+        return 0
+
+    return funcInfo_map[start_addr]["newOffset"] + (idaAddr - funcInfo_map[start_addr]["originalAddr"])
+
+
+def getDwordValue(vaule):
+    return ctypes.c_uint32(vaule).value
+
+
+def get_wide_str_length(ea):
+    length = 0
+    while ida_bytes.get_wide_byte(ea + length * 2) != 0:
+        length += 1
+    return length
+
+
+def saveFuncData(funcAddr):
+    global fileOffset
+
+    func = ida_funcs.get_func(funcAddr)
+    start_addr = func.start_ea
+    end_addr = func.end_ea
+
+    if start_addr in funcInfo_map:
+        return
+    funcInfo_map[start_addr] = AddrInfoStruct(start_addr, fileOffset)
+
+    fileOffset += (end_addr - start_addr)
+
+    shellcodeData.extend(get_bytes(start_addr, end_addr - start_addr))
+
+    func_items = FuncItems(start_addr)
+    for addr in func_items:
+        str = idc.generate_disasm_line(addr, 0)
+        if ida_bytes.get_byte(addr) == 0xE8 and ("security_check_cookie" not in str):
+            callFuncAddr = ida_bytes.get_dword(addr + 1) + addr + 5
+            saveFuncData(getDwordValue(callFuncAddr))
+
+
+def fixE8CallOffset():
+    for key, value in funcInfo_map.items():
+        func_items = FuncItems(key)
+        for addr in func_items:
+            str = idc.generate_disasm_line(addr, 0)
+            if ida_bytes.get_byte(addr) == 0xE8 and ("security_check_cookie" not in str):
+                callFuncAddr = ida_bytes.get_dword(addr + 1) + addr + 5
+                callFuncAddr = getDwordValue(callFuncAddr)
+
+                if callFuncAddr in funcInfo_map:
+                    e8CallOffset = funcInfo_map[callFuncAddr]["newOffset"] - (idaAddr2FileOffset(addr) + 5)
+                else:  # 处理特殊情况:在同一个函数中跳转指令没有用jcc实现，而是用的call指令
+                    func = ida_funcs.get_func(callFuncAddr)
+                    fixOffset = funcInfo_map[func.start_ea]["newOffset"] + (
+                                callFuncAddr - funcInfo_map[func.start_ea]["originalAddr"])
+                    e8CallOffset = fixOffset - (idaAddr2FileOffset(addr) + 5)
+                patchDword(idaAddr2FileOffset(addr + 1), getDwordValue(e8CallOffset))
+    print("--------------fixE8CallOffset finish!--------------")
+
+
+def deleteSecurityCode():
+    for key, value in funcInfo_map.items():
+        func_items = FuncItems(key)
+        for addr in func_items:
+            str = idc.generate_disasm_line(addr, 0)
+            if "security_check_cookie" in str:
+                memset(idaAddr2FileOffset(addr - 5), 0x90, 10)
+            elif "security_cookie" in str:
+                memset(idaAddr2FileOffset(addr), 0x90, 10)
+    print("--------------deleteSecurityCode finish!--------------")
+
+
+def saveVarData(addr, case):
+    global fileOffset
+
+    for i in range(5):
+        value = get_operand_value(addr, i)
+        if value < 0xf0000000 and value > 0x1000: #这里有的操作数会为负数，需要处理下
+            break
+    value = getDwordValue(value)
+
+    if value in varInfo_map:
+        return
+
+    addr_align()
+
+    varInfo_map[value] = AddrInfoStruct(value, fileOffset)
+
+    for x in XrefsTo(value, flags=0):
+        relocOffset = idaAddr2FileOffset(searchValue(x.frm, value))
+        if relocOffset != 0:
+            patchDword(relocOffset, getDwordValue(fileOffset))
+            relocTable.append(relocOffset)
+
+    if case == -1:
+        strLength = 0
+        if ida_nalt.get_str_type(value) != ida_nalt.STRTYPE_C:
+            strLength = (get_wide_str_length(value) + 1) * 2
+        else:
+            str_content = idc.get_strlit_contents(value)
+            strLength = len(str_content) + 1
+        shellcodeData.extend(get_bytes(value, strLength))
+        fileOffset += strLength
+    elif case == 5:
+        i = 0
+        length = 0
+        while i < 5:
+            addr = next_head(addr)
+            str = idc.generate_disasm_line(addr, 0)
+            if "movsw" in str:
+                length += 2
+            if "movsb" in str:
+                length += 1
+            i += 1
+        while 1:
+            addr = prev_head(addr)
+            str = idc.generate_disasm_line(addr, 0)
+            if "ecx" in str:
+                ecxValue = get_operand_value(addr, 1)
+                length += ecxValue * 4
+                break
+        shellcodeData.extend(get_bytes(value, length))
+        fileOffset += length
+    else:
+        shellcodeData.extend(get_bytes(value, 4))
+        fileOffset += 4
+
+
+def copyVarToShellcode():
+    for key, value in funcInfo_map.items():
+        func_items = FuncItems(key)
+        for addr in func_items:
+            str = idc.generate_disasm_line(addr, 0)
+            if " byte_" in str:
+                saveVarData(addr, 1)
+            elif " word_" in str:
+                saveVarData(addr, 2)
+            elif str.find("dword_") != -1:
+                saveVarData(addr, 4)
+            elif "offset" in str:
+                if "mov     esi" in str:
+                    saveVarData(addr, 5)
+                else:
+                    saveVarData(addr, -1)
+
+    addr_align()
+    print("--------------copyVarToShellcode finish!--------------")
+
+
+def imp_cb(ea, name, ord):
+    if name:
+        importInfo_map[name] = {"dllName": dllName, "apiName": name, "flag": False, "addr": ea}
+    return True
+
+
+def enum_exported_dll_func():
+    global dllName
+
+    for i in range(idaapi.get_import_module_qty()):
+        dllName = idaapi.get_import_module_name(i)
+        idaapi.enum_import_names(i, imp_cb)
+
+
+def generateIatStrToShellcode():
+    global fileOffset, shellcodeData
+
+    enum_exported_dll_func()
+    replaceMarkValue(0x77777777, fileOffset)
+
+    for key, value in funcInfo_map.items():
+        func_items = FuncItems(key)
+        for addr in func_items:
+            str = idc.generate_disasm_line(addr, 0)
+            if "call" in str and "ds:" in str:
+                apiName = str[str.rfind(":") + 1:]
+
+                if importInfo_map[apiName]["flag"] == False:
+                    importInfo_map[apiName]["flag"] = True
+                    if importInfo_map[apiName]["dllName"] not in iatInfo_map:
+                        iatInfo_map[importInfo_map[apiName]["dllName"]] = []
+                    iatInfo_map[importInfo_map[apiName]["dllName"]].append(importInfo_map[apiName]["apiName"])
+
+    for key, value in iatInfo_map.items():
+        shellcodeData += bytes(key, encoding="ascii")
+        shellcodeData += bytearray('\x00\x2C', encoding="ascii")
+        fileOffset += len(key) + 2
+        for str in value:
+            shellcodeData += bytes(str, encoding="ascii")
+            shellcodeData += bytearray('\x00\x2C', encoding="ascii")
+            fileOffset += len(str) + 2
+        shellcodeData[fileOffset - 1] = 0x3B
+    shellcodeData[fileOffset - 1] = 0x00
+
+    addr_align()
+    print("--------------generateIatStrToShellcode finish!--------------")
+
+
+def generateIatTableAndFix():
+    global fileOffset
+    replaceMarkValue(0x66666666, fileOffset)
+
+    for key, value in iatInfo_map.items():
+        for apiName in value:
+            iatInfo = importInfo_map[apiName]
+
+            for x in XrefsTo(iatInfo["addr"], flags=0):
+                func = ida_funcs.get_func(x.frm)
+                if func == None or func.start_ea not in funcInfo_map:
+                    continue
+
+                relocOffset = idaAddr2FileOffset(searchValue(x.frm, iatInfo["addr"]))
+                if relocOffset != 0:
+                    patchDword(relocOffset, getDwordValue(fileOffset))  # 这是是不是不需要getDwordValue
+                    relocTable.append(relocOffset)
+
+            shellcodeData.extend(b'\x00' * 4)
+            fileOffset += 4
+    addr_align()
+    print("--------------generateIatTableAndFix finish!--------------")
+
+
+def generateRelocTable():
+    global fileOffset, relocTable
+    replaceMarkValue(0x99999999, fileOffset)
+
+    relocTable = list(set(relocTable))
+    for relocOffset in relocTable:
+        shellcodeData.extend(struct.pack('<H', relocOffset))
+        fileOffset += 2
+
+    replaceMarkValue(0x88888888, fileOffset)
+    addr_align()
+    print("--------------generateRelocTable finish!--------------")
+
+
+def outputShellcode(outType):
+    if outType == "txt":
+        fileName = 'shellcode.txt'
+        shellcodeTxt = "\""
+        f = open(fileName, 'w')
+        for index in range(fileOffset):
+            shellcodeTxt += '\\x{:02x}'.format(shellcodeData[index])
+        shellcodeTxt += "\""
+        f.write(shellcodeTxt)
+    elif outType == "raw":
+        fileName = 'shellcode.bin'
+        f = open(fileName, 'wb')
+        f.write(shellcodeData)
+
+    f.close()
+    print("--------------outputShellcode finish!--------------")
+    print("shellcode outputPath: %s" % (os.getcwd() + '\\' + fileName))
+    print("shellcode length: 0x%x" % fileOffset)
+
+
+def main(OEP, outType):
+    saveFuncData(OEP)
+    print("--------------saveFuncData finish!--------------")
+
+    fixE8CallOffset()
+
+    deleteSecurityCode()
+
+    copyVarToShellcode()
+
+    generateIatStrToShellcode()
+
+    generateIatTableAndFix()
+
+    generateRelocTable()
+
+    outputShellcode(outType)
+
+    return
+
+
+if __name__ == '__main__':
+    main(here(), "txt")
